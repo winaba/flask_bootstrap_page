8e5a86cbd5dae90c5391a45d01b27582f0a773c0
wagner.inaba@gmail.com <wagner.inaba@gmail.com>
2019-12-31 21:04:34 -0300

    Route update

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,4 +5,4 @@ from app import app
 @app.route("/", defaults={"user": None})
 
 def index(user):
-    return render_template('base.html', user=user)
+    return render_template('index.html', user=user)
