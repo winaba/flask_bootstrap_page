fbbdf7c98c48380b5a38af3430a4c8e4581e6c05
wagner.inaba@gmail.com <wagner.inaba@gmail.com>
2020-01-04 01:02:13 -0300

    Inclusão da rota /login

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from app.models.forms import LoginForm
 
-@app.route("/index/<user>")
-@app.route("/", defaults={"user": None})
+@app.route("/index/")
+@app.route("/")
+def index():
+    return render_template('index.html')
 
-def index(user):
-    return render_template('index.html', user=user)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           form=form)
